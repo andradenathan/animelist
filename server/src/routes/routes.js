6ff207b59a5b66666dbf7f25d526aabd99e45d2f
Nathan Andrade <nathan.nhdsk@gmail.com>
2021-06-08 19:22:47 -0300

    Back-end routes

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -1,1 +1,12 @@
+const Users = require('../controllers/users');
+const { Router } = require('express');
 
+const router = Router();
+
+router.get('/users', Users.index);
+router.get('/users/:id', Users.show);
+router.put('/users/:id', Users.update);
+router.post('/users', Users.create);
+router.delete('/users/:id', Users.destroy);
+
+module.exports = router;
