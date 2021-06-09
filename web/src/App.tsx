29531ba6eba5647d3f9d4c56d65427c0a3d2f1a2
Nathan Andrade <nathan.nhdsk@gmail.com>
2021-06-09 18:27:37 -0300

    Sign up page and new register form

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,38 +1,11 @@
-import React from 'react';
 import './styles/global.css';
-import './styles/pages/home.css';
+import './pages/Home';
+
+import Routes from './routes';
 
 function App() {
   return (
-  <div id="homepage">
-      <div className="home-top-contents">
-      <h1 className="top-text">Animelist</h1>
-        <div className="home-top-right-menu">
-          <div className="menu-content">
-            <div className="signin-button">Sign In</div>
-          </div>
-
-          <div className="menu-content">
-          <div className="signup-button">Sign Up</div>
-          </div>
-
-        </div>
-      </div>
-     
-      <div className="home-body-section">
-        <div className="home-body-content">
-          <h1>Most Popular Animes</h1>
-          <div className="home-body-content-box">
-            <div className="home-body-slide">
-              quadrado aqui
-            </div>
-            <div className="home-body-slide-description">
-              descrição aqui
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <Routes />
   );
 }
 
---
+++ web/src/routes.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Switch, Route} from 'react-router-dom';
+
+import Home from './pages/Home';
+import Register from './pages/Register/index';
+
+function Routes() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/register" component={Register} />
+            </Switch>
+        </BrowserRouter>
+    );
+}
+
+export default Routes;
